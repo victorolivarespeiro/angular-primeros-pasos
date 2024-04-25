e03f036bd97a0d02df92a41a3745befa3f8ac26e
Víctor Olivares <victorolivarespeiro@gmail.com>
2024-04-25 12:43:51 +0200

    Fin de sección 4

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+
+  public name: string = 'ironman';
+  public age: number = 45;
+
+  get capitalizedName ():string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription():string{
+    return `${this.name} - ${this.age}`;
+  }
+  //cambia nombre a spiderman
+  changeHero():void {
+    this.name = 'Spiderman';
+  }
+  //cambia edad
+  changeAge():void {
+    this.age = 25;
+  }
+  //resetea web
+  resetForm():void {
+    this.name = 'ironman',
+    this.age = 45
+
+    // document.querySelectorAll('h1')!.forEach (element => {
+    //   element.innerHTML = '<h1>Desde Angular</h1>';
+    // })
+  }
+}
